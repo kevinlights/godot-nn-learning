1821cb5f5b810c999cb8c490d6b5da371aa36ebb
kevinlights <guangjinhai@163.com>
2024-05-15 22:23:21 +0800

    world game init

---
+++ games/world/biologic/Omnivore.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+# 杂食动物
+class_name Omnivore
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
