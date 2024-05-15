8a74f5d4cef7680c7110ce303fb9308ae4bf5957
kevinlights <guangjinhai@163.com>
2024-05-15 18:10:26 +0800

    update folder structure; add git plugin

---
+++ games/ping_pong/PlayerScene.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+# this scene node is just for training
+# 仅用于训练，后续可用 Player 来接球
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_area_entered(ball: Ball):
+	ball.horizontal = -(ball.horizontal)
+	# 给球一些随机速度补偿，加快训练速度
+	ball.vel = randf_range(0.8, 1.5) * ball.vel
---
+++ games/ping_pong/player.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+
+var racket
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	racket = Racket.new(self)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_area_entered(ball: Ball):
+	racket.hit(ball)
